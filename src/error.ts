f5e17b1c4c696a557f57a2ce4e49ace7e957e0a4
Ceale <clxh100100@hotmail.com>
2025-09-10 03:52:48 +0800

    feat(util): 升级版本并优化 tryCatch 函数
    - 将版本号从 1.12.1 升级到 1.12.2
    - 优化 tryCatch 函数，使其返回值同时支持对象属性访问和数组索引访问
    - 增加了 tryCatch 函数的类型注释和示例说明
    - 优化了错误提示信息

--- src/error.ts
+++ src/error.ts
@@ -1,12 +1,18 @@
 type Success<T> = {
     data: T
     error: null
-}
+} & [
+    data: T,
+    error: null
+]
 
 type Failure<E> = {
     data: null
     error: E
-}
+} & [
+    data: null,
+    error: E
+]
 
 type Result<T, E extends unknown = Error> = Success<T> | Failure<E>
 
@@ -15,27 +21,47 @@ interface tryCatch {
     <V>(arg: (() => V)): Result<V>
 }
 
+/**
+ * 尝试执行一个函数或Promise，返回一个包含执行结果或错误信息的复合对象
+ * 
+ * 该对象同时支持对象属性访问(.data/.error)和数组索引访问([0]/[1])两种方式
+ * 
+ * @param parameter 一个函数或者Promise对象
+ * @returns 返回参数中，data和[0]是函数的返回值或是Promise的解析结果，error和[1]是错误对象。两者将有一为`null`
+ * 
+ * @example
+ * tryCatch(() => func(...))
+ * // => [data, error] & {data, error}
+ * // data: func的返回值，error：函数执行过程中发生的错误
+ * 
+ * @example
+ * await tryCatch(asyncFunc(...))
+ * // => [data, error] & {data, error}
+ * // data: asyncFunc的返回值，error：asyncFunc执行过程中发生的错误
+ */
 export const tryCatch: tryCatch = (parameter: any): any => {
     // 处理 Promise
     if (parameter instanceof Promise) {
         return parameter
-            .then(data => ({ data, error: null }))
-            .catch(error => ({ data: null, error }))
+            .then(data => Object.assign([ data, null ], { data, error: null }))
+            .catch(error => Object.assign([ null, error ], { data: null, error }))
     }
     
     // 处理函数
     if (typeof parameter === "function") {
         try {
-            return { data: parameter(), error: null }
+            const data = parameter()
+            return Object.assign([ data, null ], { data, error: null })
         } catch (error) {
-            return { data: null, error }
+            return Object.assign([ null, error ], { data: null, error })
         }
     }
     
-    throw new TypeError("参数类型错误，必须是 Promise 或函数")
+    throw new TypeError("参数类型错误，应为 Promise 或函数")
 }
 
 // const a = tryCatch(() => {})
+// console.log(a)
 // if (!a.error) {
 //     console.log(a.error)
 // } else {
