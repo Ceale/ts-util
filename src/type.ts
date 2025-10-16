30d3218d20c5a7611943ecd82584b329fdc1ff20
Ceale <clxh100100@hotmail.com>
2025-10-16 16:58:48 +0800

    fix(type): 更新断言函数参数名称
    将 `assert` 函数的参数名从 `target` 更改为 `variable`，使语义更清晰。
    同时更新了对应的 JSDoc 注释中的参数描述。
    
    chore(package): 升级版本号至 1.13.2
    
    更新 package.json 中的版本字段，准备发布新版本。

--- src/type.ts
+++ src/type.ts
@@ -1,6 +1,6 @@
 /**
  * 就地断言一个变量为指定类型
  * @template Type 断言的类型，默认为`any`
- * @param target 待断言的变量
+ * @param variable 待断言的变量
  */
-export const assert = <Type = any>(target: any): asserts target is Type => {}
+export const assert = <Type = any>(variable: any): asserts variable is Type => {}
