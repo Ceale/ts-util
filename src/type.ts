eca7a2a9aa4af5eadba50e5b9c2ec56e52645226
Ceale <clxh100100@hotmail.com>
2025-10-16 16:54:06 +0800

    fix(type): 更新assert函数类型参数名称并添加注释
    将类型参数 `T` 重命名为 `Type` 以提高可读性，并为 `assert` 函数添加了详细的
    JSDoc 注释，说明其用途和参数。同时更新了包版本号。

--- src/type.ts
+++ src/type.ts
@@ -1,1 +1,6 @@
-export const assert = <T = any>(v: any): asserts v is T => {}
+/**
+ * 就地断言一个变量为指定类型
+ * @template Type 断言的类型，默认为`any`
+ * @param target 待断言的变量
+ */
+export const assert = <Type = any>(target: any): asserts target is Type => {}
