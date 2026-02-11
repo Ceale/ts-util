f2cb7be4ffe618a827d2920bb5be80901d622f5b
Ceale <clxh100100@hotmail.com>
2026-02-11 18:54:47 +0800

    破坏性更改：分离原 enum 功能到enum_v1，新增了全新的 enum_v2，原 enum 现在以 EnumV1 的名称导出

---
+++ src/enum_v2.ts
@@ -0,0 +1,38 @@
+import type { anyobject } from "./type"
+
+type EnumKeyValue<T extends readonly string[]> = {
+    [K in T[number] as Uppercase<K>]: K
+}
+
+interface EnumMethod<T extends readonly string[]> {
+    values(): T
+    includes<V extends any>(value: V): boolean
+}
+
+/**
+ * 提取枚举对象中的枚举值
+ */
+export type EnumOf<T extends Record<string, any>> = Extract<T[keyof T], string>
+
+/**
+ * 创建一个枚举对象
+ * @param 若干个枚举值，字符串
+ * @returns 枚举对象，包含枚举值和枚举方法，枚举键为枚举值的大写
+ * 
+ * @example
+ * const Example = Enum("value_a", "value_b")
+ * // {
+ * //    VALUE_A: "value_a",
+ * //    VALUE_B: "value_b"
+ * //    values(): ["value_a", "value_b"],
+ * //    includes(value: string): boolean
+ * // }
+ * 
+ */
+export const Enum = <T extends readonly string[]>(...values: T) => {
+    const obj: anyobject = {}
+    values.forEach(value => obj[value.toUpperCase()] = value)
+    obj.values = () => values
+    obj.includes = (value: string) => values.includes(value)
+    return obj as EnumKeyValue<T> & EnumMethod<T>
+}
